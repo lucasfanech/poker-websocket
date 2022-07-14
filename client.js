bab5d7f17a865c89b006199fc7c47107097032e6
lucas.fanech <fanech.lucas@gmail.com>
2022-07-14 19:13:34 +0200

    avancées chat

--- client.js
+++ client.js
@@ -1,8 +1,17 @@
 // include websocket client
 var WebSocket = require('ws');
-
+var username ="";
 // connect to websocket server
 var ws = new WebSocket('ws://localhost:8080');
+console.log('Entrez un nom d\'utilisateur');
+process.stdin.on('data', function(data) {
+    if (username === "") {
+        username = data.toString().trim();
+        ws.send('Client s\'est connecté: ' + username);
+    }
+}
+
+);
 // listen for messages
 ws.on('message', function(message) {
   console.log('received: %s', message);
@@ -10,16 +19,10 @@ ws.on('message', function(message) {
 );
 // on write, send message
 ws.on('open', function() {
-    var username= '';
-    console.log('Entrez un nom d\'utilisateur');
-    process.stdin.on('data', function(data) {
-        username = data.toString().trim();
-        }
-    );
-    ws.send('Client s\'est connecté: ' + username);
+
     // ecoute ecriture dans le terminal
     process.stdin.on('data', function(data) {
-        ws.send(data);
+        ws.send(username+": "+data);
     }
     );
 }
--- server.js
+++ server.js
@@ -4,8 +4,17 @@ var wss = new WebSocketServer({port: 8080});
 // start Websocket server
 wss.on('connection', function(ws) {
   ws.on('message', function(message) {
-    console.log('received: %s', message);
+    // if message isn't empty and equals to ""
+    if (message && message !== "") {
+      console.log('>%s', message);
+
+      // send message to all clients
+        wss.clients.forEach(function(client) {
+            client.send('>'+message);
+        }
+        );
+    }
   });
-  //ws.send('something');
+
 }
 );
